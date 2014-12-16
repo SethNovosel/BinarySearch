39f174624c85ec02f9b2c0cb51995bd287b7e28a
Seth.Novosel <Seth.Novosel@MU-C3-14.mullen.pvt.k12.co.us>
2014-12-16 09:00:54 -0700

    new commit

---
+++ BinarySearch/src/BinarySearch.java
@@ -0,0 +1,57 @@
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class BinarySearch
+	{
+	static String answer;
+	static int num;
+	static boolean playing = true;
+	public static void main(String[] args)
+		{
+		System.out.println("This is a binary search quiz.");
+		System.out.println("Enter in numbers.");
+		ArrayList elem = new ArrayList();
+		do
+			{
+			Scanner userInput = new Scanner(System.in);
+			num = userInput.nextInt();
+			if(num == -1)
+				{
+				
+				}
+			for(Object x : elem)
+				{
+				System.out.println(x);
+				}
+			}
+		while(playing);
+		}
+	
+	
+	
+	
+	
+	public static int BinarySearch(int[] elements, int target)
+		{
+		Collections.sort(elements);
+		int left = 0;
+		int right = elements.length - 1;
+		while (left <= right)
+			{
+			int middle = (left + right) / 2;
+			if (target < elements[middle])
+				{
+				right = middle - 1;
+				}
+			else if (target > elements[middle])
+				{
+				left = middle + 1;
+				}
+			else
+				{
+				return middle;
+				}
+			}
+		return - 1;
+		}
+	}
